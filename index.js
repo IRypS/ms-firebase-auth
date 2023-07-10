8635f22fcb18ac93b96207c9e2c06b8361c90d6c
IRypS <pelaezricardo1502@gmail.com>
2023-07-10 11:59:40 -0500

    add workflow

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 import express from 'express';
+const app = express();
+
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
+import cors from 'cors';
+
 
 import rutasApp from './controller/routes.js';
 
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true }) )
-
+app.use( cors() );
 app.use('/', rutasApp);
 
 const PORT = process.env.PORT || 9050;
 
 app.listen(
   PORT,
+  '0.0.0.0',
   () => {
     console.log(`📗 La app está funcionando en el puerto: http://localhost:${PORT}`);
   }
